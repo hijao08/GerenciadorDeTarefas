4700d544c39bfdae7cea83aff92dc378b2dc97b0
João Vitor Soares Da Silva <jvsoares082004@gmail.com>
2025-06-16 23:25:34 -0300

    em desenvolvimento

--- src/pilha.cpp
+++ src/pilha.cpp
@@ -1,46 +1,64 @@
 #include "pilha.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void inicializar_pilha(Pilha* pilha) {
-    pilha->elementos = (Tarefa**)malloc(MAX_PILHA * sizeof(Tarefa*));
-    if (pilha->elementos == NULL) {
-        printf("Erro ao alocar memória para a pilha!\n");
-        exit(1);
-    }
+    pilha->elementos = (Acao**)malloc(MAX_PILHA * sizeof(Acao*));
     pilha->topo = -1;
     pilha->capacidade = MAX_PILHA;
 }
 
 void destruir_pilha(Pilha* pilha) {
+    while (!pilha_vazia(pilha)) {
+        Acao* acao = desempilhar(pilha);
+        free(acao);
+    }
     free(pilha->elementos);
     pilha->elementos = NULL;
     pilha->topo = -1;
     pilha->capacidade = 0;
 }
 
-void empilhar(Pilha* pilha, Tarefa* elemento) {
-    if (!pilha_cheia(pilha)) {
-        pilha->topo++;
-        pilha->elementos[pilha->topo] = elemento;
+void empilhar(Pilha* pilha, TipoAcao tipo, Tarefa* tarefa) {
+    if (pilha->topo < pilha->capacidade - 1) {
+        Acao* nova_acao = (Acao*)malloc(sizeof(Acao));
+        nova_acao->tipo = tipo;
+        nova_acao->tarefa = tarefa;
+        pilha->elementos[++pilha->topo] = nova_acao;
     } else {
         printf("Erro: Pilha cheia!\n");
     }
 }
 
-Tarefa* desempilhar(Pilha* pilha) {
-    if (!pilha_vazia(pilha)) {
-        Tarefa* elemento = pilha->elementos[pilha->topo];
-        pilha->topo--;
-        return elemento;
+void empilhar_edicao(Pilha* pilha, Tarefa* tarefa, const char* titulo_anterior, 
+                     const char* descricao_anterior, const char* data_anterior, 
+                     int prioridade_anterior, int concluida_anterior) {
+    if (pilha->topo < pilha->capacidade - 1) {
+        Acao* nova_acao = (Acao*)malloc(sizeof(Acao));
+        nova_acao->tipo = ACAO_EDITAR;
+        nova_acao->tarefa = tarefa;
+        strcpy(nova_acao->titulo_anterior, titulo_anterior);
+        strcpy(nova_acao->descricao_anterior, descricao_anterior);
+        strcpy(nova_acao->data_anterior, data_anterior);
+        nova_acao->prioridade_anterior = prioridade_anterior;
+        nova_acao->concluida_anterior = concluida_anterior;
+        pilha->elementos[++pilha->topo] = nova_acao;
+    } else {
+        printf("Erro: Pilha cheia!\n");
     }
-    printf("Erro: Pilha vazia!\n");
+}
+
+Acao* desempilhar(Pilha* pilha) {
+    if (!pilha_vazia(pilha)) {
+        return pilha->elementos[pilha->topo--];
+    }
     return NULL;
 }
 
 Tarefa* topo_pilha(Pilha* pilha) {
     if (!pilha_vazia(pilha)) {
-        return pilha->elementos[pilha->topo];
+        return pilha->elementos[pilha->topo]->tarefa;
     }
     return NULL;
 }
--- src/pilha.h
+++ src/pilha.h
@@ -6,20 +6,38 @@
 
 #define MAX_PILHA 100
 
+typedef enum {
+    ACAO_CRIAR,
+    ACAO_EDITAR,
+    ACAO_REMOVER,
+    ACAO_CONCLUIR
+} TipoAcao;
+
 typedef struct {
-    Tarefa** elementos;
+    TipoAcao tipo;
+    Tarefa* tarefa;
+    // Para edição, guardar estado anterior
+    char titulo_anterior[MAX_TITULO];
+    char descricao_anterior[MAX_DESC];
+    char data_anterior[MAX_DATA];
+    int prioridade_anterior;
+    int concluida_anterior;
+} Acao;
+
+typedef struct {
+    Acao** elementos;
     int topo;
     int capacidade;
 } Pilha;
 
 void inicializar_pilha(Pilha* pilha);
 void destruir_pilha(Pilha* pilha);
-void empilhar(Pilha* pilha, Tarefa* elemento);
-Tarefa* desempilhar(Pilha* pilha);
-Tarefa* topo_pilha(Pilha* pilha);
+void empilhar(Pilha* pilha, TipoAcao tipo, Tarefa* tarefa);
+Acao* desempilhar(Pilha* pilha);
 int pilha_vazia(Pilha* pilha);
-int pilha_cheia(Pilha* pilha);
-int tamanho_pilha(Pilha* pilha);
+void empilhar_edicao(Pilha* pilha, Tarefa* tarefa, const char* titulo_anterior, 
+                     const char* descricao_anterior, const char* data_anterior, 
+                     int prioridade_anterior, int concluida_anterior);
 
 #endif
 
